33aa9fcdd01d08b1d1703fac0fc44625381b4b04
Xavier Reyes Ochoa <academia.x.edu@gmail.com>
2021-11-07 16:00:32 -0800

    estructura de meta

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Encabezado from './componentes/compartidos/Encabezado';
 import Pie from './componentes/compartidos/Pie';
 import Principal from './componentes/compartidos/Principal';
+import Meta from './componentes/lista/Meta';
 
 function App() {
   return (
     <div className="App">
       <Encabezado></Encabezado>
-      <Principal></Principal>
+      <Principal>
+        <Meta></Meta>
+      </Principal>
       <Pie></Pie>
     </div>
   );
---
+++ src/componentes/lista/Meta.js
@@ -0,0 +1,22 @@
+function Meta() {
+    return (
+        <div>
+            <div>
+                <div>icono</div>
+                <p>1<sub>/ semana</sub></p>
+                <p>detalles</p>
+            </div>
+            <div>
+                <div>
+                    <p>4 de 5</p>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+                <button>Completado</button>
+            </div>
+        </div>
+    );
+}
+
+export default Meta;
